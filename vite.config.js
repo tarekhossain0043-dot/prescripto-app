9507d7867fcd21c2b8225109655abf7a7af6c9b2
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-10-15 21:32:15 +0600

    feat: gh-pages usees for deployment on github

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+const deployApp = "prescripto-app";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${deployApp}`,
+});
